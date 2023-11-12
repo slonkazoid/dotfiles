876a3b90ad788ef3b8b29f71a83db1368b389c27
slonkazoid <alifurkanyildiz@gmail.com>
2023-11-12 18:17:53 +0300

    refactor record.sh (thanks sewn)

--- .local/bin/record.sh
+++ .local/bin/record.sh
@@ -2,43 +2,42 @@
 
 # long live avt
 
+set -e
+
 video_path="${1:-$HOME/Desktop/"$(date '+%Y-%m-%d %H-%M-%S')".mkv}"
+thumbnail_path="/tmp/thumb_$(basename "${video_path}")_$RANDOM.png"
 
-notify_end() {
-	image_path="/tmp/thumb_$(basename "${video_path}")_$RANDOM.png"
-	if ffmpeg -hide_banner -i "${video_path}" -frames:v 1 "${image_path}" &>/dev/null
-	then could_make_image=
-	else echo "error while making thumbnail" >&2
-	fi
+generate_thumbnail() {
+	ffmpeg -hide_banner -i "$1" -frames:v 1 "$2" &>/dev/null
+}
 
-	case "$(
+notify() {
+	local thumbnail
+	generate_thumbnail "$video_path" "$thumbnail_path" &&
+		thumbnail=1 ||
+		echo "error while making thumbnail" >&2
+
 	notify-send 'Video recorded' "Saved to '${video_path}'" \
-		${could_make_image+-i "${image_path}"} \
+		${thumbnail:+-i "$thumbnail_path"} \
 		-a 'wf-recorder' -t 5000 -w \
-		-A "view=View" -A "delete=Delete" \
-	)" in
-	view)
-		xdg-open "${video_path}"
-		;;
-	delete)
-		rm "${video_path}"
-		;;
-	esac
-	rm "${image_path}"
-	echo "${video_path}"
+		-A "view=View" -A "delete=Delete"
+
+	[[ "$thumbnail" == 1 ]] && rm "$thumbnail_path"
 }
 
-findmonitor() {
-	echo "$(pactl list cards | grep -oPm1 'Name: \K[^\s]+').monitor"
-}
+notify-send "Recording screen" "Recording video to '${video_path}'" &
 
 wf-recorder \
 	-x rgb24 -c libx264rgb \
 	-p crf=14 -p level=5.2 -p preset=veryfast -p profile=high444 \
-	-f "${video_path}" >&2 && notify_end || exit $? &
+	-f "$video_path" >&2 || {
+	notify-send "Couldn't record screen" "wf-recorder exited with code $?" &
+	exit 1
+}
 
-pid=$!
+echo "$video_path"
 
-notify-send "Recording screen" "Recording video to '${video_path}'"
-
-kill -0 $pid 2>/dev/null && wait $pid
+case "$(notify)" in
+view) xdg-open "$video_path" ;;
+delete)	rm "$video_path" ;;
+esac
