63026ec76a80b8b1823567c5cd239eb15f9b729b
slonkazoid <slonkazoid@slonk.ing>
2025-02-06 14:55:59 +0300

    add rounding to avoid floating point issues

--- .local/bin/mic.sh
+++ .local/bin/mic.sh
@@ -3,5 +3,5 @@
 if [[ "$(pactl get-source-mute @DEFAULT_SOURCE@)" == "Mute: yes" ]]; then
 	echo  Mute
 else
-	echo  "$(wpctl get-volume @DEFAULT_SOURCE@ | cut -f2 -d' ' | jq -r '. * 100')"%
+	echo  "$(wpctl get-volume @DEFAULT_SOURCE@ | cut -f2 -d' ' | jq -r '. * 1000 | round / 10')"%
 fi
